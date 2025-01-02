fd9fb45eee574c01feb24da7497c5e9fb83372dc
noamguterman <noamguterman@gmail.com>
2025-01-02 11:45:02 +0200

    refactor

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
+document.getElementById('btn-convert').addEventListener('click', convert)
+
 function convert() {
-  const inputValue = document.getElementById("userInput").value;
-  const unit = document.getElementById("unit").value;
-  const milesToKm = (unit === "milesToKm");
-  let result = 0;
+  const inputValue = document.getElementById("userInput").value
+  const unit = document.getElementById("unit").value
+  const milesToKm = (unit === "milesToKm")
+  let result = 0
+
   if (milesToKm === true) {
-    result = inputValue * 1.60934;
+    result = (inputValue * 1.60934).toFixed(2)
   } else {
-    result = inputValue / 1.60934;
+    result = (inputValue / 1.60934).toFixed(2)
   }
-  document.getElementById("resultElement").innerHTML = result;
+
+  document.getElementById("resultElement").innerHTML = result
 }
